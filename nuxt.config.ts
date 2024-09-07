969b3a28a4ecd5d3d9a310c79325f8e5d6bd625b
CodeSyncr <yashkumar12125@gmail.com>
2024-09-07 15:52:24 +0530

    fix: deployed site

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,18 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
-  extends: ['@nuxt-themes/docus'],
+  extends: ["@nuxt-themes/docus"],
 
   devtools: { enabled: true },
+
+  app: {
+    baseURL: "/docs/", // Set the base URL for GitHub Pages
+  },
 
   modules: [
     // Remove it if you don't use Plausible analytics
     // https://github.com/nuxt-modules/plausible
-    '@nuxtjs/plausible'
+    "@nuxtjs/plausible",
   ],
-
-  compatibilityDate: '2024-09-07'
-})
+  ssr: false,
+  compatibilityDate: "2024-09-07",
+});
