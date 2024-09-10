da59318be7c65353bd20cdf24edd6b1e6c40a248
CodeSyncr <yashkumar12125@gmail.com>
2024-09-11 01:51:07 +0530

    fix: build error on base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,12 @@
 export default defineNuxtConfig({
-  // https://github.com/nuxt-themes/docus
   extends: ["@nuxt-themes/docus"],
 
   devtools: { enabled: true },
 
   app: {
-    baseURL: ".", // Set the base URL for GitHub Pages
+    baseURL: "/", // Set the base URL for GitHub Pages
   },
 
-  modules: [
-    // Remove it if you don't use Plausible analytics
-    // https://github.com/nuxt-modules/plausible
-    "@nuxtjs/plausible",
-  ],
+  modules: ["@nuxtjs/plausible"],
   compatibilityDate: "2024-09-07",
 });
